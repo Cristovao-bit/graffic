e4dceb0c6788bb3a2ee692dce348bb9a249339c3
Cristovao-bit <63509410+Cristovao-bit@users.noreply.github.com>
2020-06-15 00:05:19 -0300

    origin

---
+++ graffic_01/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Grafico de Barra vertical</title>
+        <link rel="stylesheet" href="_css/style.css"/>
+    </head>
+    <body>
+        <section>
+            <div class="box">
+                <div class="skill">
+                    <div class="graph" style="height: 85%;">
+                        <div class="percent">85%</div>
+                    </div>
+                    <div class="name">HTML</div>
+                </div>
+                <div class="skill">
+                    <div class="graph" style="height: 92%;">
+                        <div class="percent">92%</div>
+                    </div>
+                    <div class="name">CSS</div>
+                </div>
+                <div class="skill">
+                    <div class="graph" style="height: 80%;">
+                        <div class="percent">80%</div>
+                    </div>
+                    <div class="name">JavaScript</div>
+                </div>
+                <div class="skill">
+                    <div class="graph" style="height: 75%;">
+                        <div class="percent">75%</div>
+                    </div>
+                    <div class="name">Photoshop</div>
+                </div>
+                <div class="skill">
+                    <div class="graph" style="height: 95%;">
+                        <div class="percent">95%</div>
+                    </div>
+                    <div class="name">Illustrator</div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
---
+++ graffic_02/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Grafico de Pizza</title>
+        <link rel="stylesheet" href="_css/style.css"/>
+    </head>
+    <body>
+        <div class="center">
+            <h1><span>Estatísticas de navegadores:</span></h1>
+            <div class="skillbox">
+                <p>Chrome</p>
+                <p>1280 Visistas</p>
+                <div class="skill">
+                    <div class="skill_level" style="width: 60%;"><span>60%</span></div>
+                </div>
+            </div>
+            <div class="skillbox">
+                <p>Firefox</p>
+                <p>800 Visistas</p>
+                <div class="skill">
+                    <div class="skill_level" style="width: 20%;"><span>20%</span></div>
+                </div>
+            </div>
+            <div class="skillbox">
+                <p>IE</p>
+                <p>100 Visitas</p>
+                <div class="skill">
+                    <div class="skill_level" style="width: 15%;"><span>15%</span></div>
+                </div>
+            </div>
+            <div class="skillbox">
+                <p>Outros</p>
+                <p>20 Visistas</p>
+                <div class="skill">
+                    <div class="skill_level" style="width: 5%;"><span>5%</span></div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
